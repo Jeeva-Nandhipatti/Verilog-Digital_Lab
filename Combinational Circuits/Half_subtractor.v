ac07bfe6d69564a68ad453bc0f95d699b5f5a164
Jeeva Nandhipatti <jeevanandhipatti@gmail.com>
2026-02-21 19:15:53 +0530

    Improve comments and format test cases in Half_subtractor
    Updated comments for clarity and modified test cases format.

--- Combinational Circuits/Half_subtractor.v
+++ Combinational Circuits/Half_subtractor.v
@@ -4,7 +4,7 @@
 
 module half_subtractor (
   input a,b,
-  output borrow,diff);
+  output borrow,diff);      //Borrow and Difference
   
   assign borrow = !a & b;
   assign diff = (!a & b) | (a & !b);
@@ -16,15 +16,16 @@ endmodule
 
 module tb;
   reg a,b;
-  wire borrow,diff;
+  wire borrow,diff;        //Borrow and Difference
   
-  half_subtractor dut(.a(a),.b(b),.borrow(borrow),.diff(diff));    //Instantiation of design file
+  half_subtractor dut(.a(a),.b(b),.borrow(borrow),.diff(diff));    //Instantiation of design module
   
   initial begin
-    a=0; b=0; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
-    a=0; b=1; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
-    a=1; b=0; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
-    a=1; b=1; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
+    //Test cases
+    a= 1'b0; b= 1'b0; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
+    a= 1'b0; b= 1'b1; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
+    a= 1'b1; b= 1'b0; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
+    a= 1'b1; b= 1'b1; #10 $display("a= %b, b= %b\nBorrow = %b, Difference = %b",a,b,borrow,diff);
     $finish;
   end
   
